d18df8baf21d99f236768b6f174c5d9e5646d857
Barabas Balazs <barabas.balazska@gmail.com>
2023-05-25 17:48:14 +0300

    Added go routines for faster scrapping

--- main.go
+++ main.go
@@ -2,13 +2,20 @@ package main
 
 import (
 	"encoding/json"
+	"io"
 	"log"
+	"net/http"
 	"os"
+	"time"
 
 	"github.com/gocolly/colly"
 )
 
-func main() {
+func ScrapeCountry(uri string) {
+
+	defer func() {
+		log.Println("Finished scraping " + uri)
+	}()
 	// Instantiate default collector
 	c := colly.NewCollector()
 
@@ -27,17 +34,38 @@ func main() {
 		})
 	})
 
-	// Before making a request print "Visiting ..."
-	c.OnRequest(func(r *colly.Request) {
-		log.Println("Visiting", r.URL.String())
-	})
-
-	c.Visit("https://www.cia.gov/the-world-factbook/countries/argentina")
+	c.Visit("https://www.cia.gov/the-world-factbook" + uri)
 
 	content, err := json.Marshal(countryInformation)
 	if err != nil {
 		log.Println(err.Error())
 	}
 
-	os.WriteFile("country.json", content, 0644)
+	countryName := uri[11 : len(uri)-1]
+
+	os.WriteFile("results/"+countryName+".json", content, 0644)
+}
+
+func main() {
+	res, err := http.Get("https://www.cia.gov/the-world-factbook/page-data/countries/page-data.json")
+	if err != nil {
+		log.Println(err.Error())
+	}
+	defer res.Body.Close()
+
+	resBody, err := io.ReadAll(res.Body)
+	if err != nil {
+		log.Println(err.Error())
+	}
+
+	var jsonData interface{}
+	json.Unmarshal(resBody, &jsonData)
+
+	data := jsonData.(map[string]interface{})["result"].(map[string]interface{})["data"].(map[string]interface{})["countries"].(map[string]interface{})["edges"].([]interface{})
+
+	for _, country := range data {
+		countryUri := country.(map[string]interface{})["node"].(map[string]interface{})["uri"].(string)
+		go ScrapeCountry(countryUri)
+	}
+	time.Sleep(10 * time.Second)
 }
